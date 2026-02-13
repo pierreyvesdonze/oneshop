e9c88330adb88c1bdf549f38882dc330079ec22f
py <pyd3.14@gmail.com>
2026-02-13 16:44:11 +0100

    Ajout Bannière promo + module sass (npm install)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/nav";
 import Home from "./components/home";
 import Test from "./components/test";
+import Banner from "./components/nav/banner";
 
 function App() {
   return (
     <Router>
       <Nav />
+      <Banner />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/test" element={<Test />} />
---
+++ frontend/src/components/nav/banner.jsx
@@ -0,0 +1,16 @@
+export default function Banner() {
+  return (
+    <div style={{
+      backgroundColor: "#000000",
+      width: "100%",
+      height: "20px",
+      color: "white",
+      textAlign: "center",
+      padding: "1rem",
+      fontWeight: "bold",
+      fontSize: "1.2rem"
+    }}>
+      50% off + free shipping
+    </div>
+  );
+}
