4627e96e598073237ea5fd47f3bb7050c29e47bd
py <pyd3.14@gmail.com>
2026-02-13 18:53:11 +0100

    broderie sur la homepage

--- frontend/src/components/home/homeImages.jsx
+++ frontend/src/components/home/homeImages.jsx
@@ -1,5 +1,5 @@
-import { motion } from "framer-motion";     
-import img1 from "../../../public/assets/images/promo/adidas-1.jpg";   
+import { motion } from "framer-motion";
+import img1 from "../../../public/assets/images/promo/adidas-1.jpg";
 import img2 from "../../../public/assets/images/promo/adidas-2.jpg";
 import img3 from "../../../public/assets/images/promo/adidas-3.jpg";
 import img4 from "../../../public/assets/images/promo/adidas-4.jpg";
@@ -7,9 +7,6 @@ import img4 from "../../../public/assets/images/promo/adidas-4.jpg";
 function HomeImages() {
   return (
     <div style={{ maxWidth: "1200px", margin: "0 auto", padding: "2rem" }}>
-      {/* Image principale */}
-      
-
       {/* Grille 2x2 pour les petites images */}
       <div
         className="grid"
@@ -35,6 +32,42 @@ function HomeImages() {
           </motion.div>
         ))}
       </div>
+
+      {/* Image bannière démo */}
+      <motion.img
+        src="/assets/images/promo/maece.jpg"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.8 }}
+        transition={{ duration: .6 }}
+        style={{
+          width: "100%",
+          height: "300px",
+          objectFit: "cover",
+          borderRadius: "10px",
+          marginBottom: "2rem",
+          display: "block",
+          marginTop: "20px",
+        }}
+      />
+
+      {/* Image Dr Marteens */}
+      <motion.img
+        src="/assets/images/promo/dr-mart-1.jpg"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.6 }}
+        transition={{ duration: .6 }}
+        style={{
+          width: "100%",
+          height: "600px",
+          objectFit: "cover",
+          borderRadius: "10px",
+          marginBottom: "2rem",
+          display: "block",
+          marginTop: "20px",
+        }}
+      />
     </div>
   );
 }
